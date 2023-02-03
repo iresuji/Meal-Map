802cbdf8a1a5c6b8c14bd85c605f10d007a86f7f
fofo20 <fuadchoaibi@hotmail.com>
2023-02-03 19:25:34 +0000

    JS adding the restaurant options

--- assets/script.js
+++ assets/script.js
@@ -19,13 +19,16 @@ const settings = {
 	"url": "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation?query="+ city,
 	"method": "GET",
 	"headers": {
-		"X-RapidAPI-Key": "7c18d7228fmshe2a6bccc38f0126p1b9546jsne2cdd20b25b0",
+		"X-RapidAPI-Key": "68792a8c29msh7c9d9f267e046f3p104235jsne344dbf0c883",
 		"X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com"
 	}
 };
 
 $.ajax(settings).done(function (response) {
 	console.log(response);
+
+
+
 
   let searchId = response.data[0].locationId;
 console.log(searchId);
@@ -36,33 +39,55 @@ const settings2 = {
     "url": "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=" + searchId,
     "method": "GET",
     "headers": {
-        "X-RapidAPI-Key": "7c18d7228fmshe2a6bccc38f0126p1b9546jsne2cdd20b25b0",
+        "X-RapidAPI-Key": "68792a8c29msh7c9d9f267e046f3p104235jsne344dbf0c883",
         "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com"
     }
+
+
     };
     $.ajax(settings2).done(function (response) {
         console.log(response);
-        let restaurantId = response.data.data[0].restaurantsId;
+ for (i=0; response.data.data.length >i; i++ ){
+var listOfRest = response.data.data[i];
+var typeFood = listOfRest.establishmentTypeAndCuisineTags[0];
+var nameRest = listOfRest.name;
+var cityRest = listOfRest.parentGeoName;
+var imageRest = listOfRest.heroImgUrl;
+var rateRest = listOfRest.averageRating;
+var restaurantId = listOfRest.restaurantsId;
 
-   // Third query to get details
-        const settings3 = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/getRestaurantDetails?restaurantsId=" + restaurantId,
-            "method": "GET",
-            "headers": {
-                "X-RapidAPI-Key": "7c18d7228fmshe2a6bccc38f0126p1b9546jsne2cdd20b25b0",
-                "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com"
-            }
-        };
-        
-        $.ajax(settings3).done(function (response) {
-            console.log(response);
-        });
-   
+$()
+<!-- <div class="child">
+<img class="child-img" src="./emma.jpg" alt="image" />
+</div>
+
+
+ }
+
+
+
+
+        ;
+
     });
 });
 })
 
+
+//    // Third query to get details
+//    const settings3 = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/getRestaurantDetails?restaurantsId=" + restaurantId,
+//     "method": "GET",
+//     "headers": {
+//         "X-RapidAPI-Key": "68792a8c29msh7c9d9f267e046f3p104235jsne344dbf0c883",
+//         "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com"
+//     }
+// };
+
+// $.ajax(settings3).done(function (response) {
+//     console.log(response);
+// });
 
 
